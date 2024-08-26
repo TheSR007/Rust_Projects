8ad3935820feb5a85bde72b9c0e8c4342bde36c4
TheSR <TheSR709@gmail.com>
2024-08-26 21:27:08 +0600

    Added Database support for Server_Example

--- Server_Example/src/main.rs
+++ Server_Example/src/main.rs
@@ -1,5 +1,21 @@
-use rocket::{get, post, launch, routes};
-use ws;
+use rocket::{get, post, routes};
+use rocket_ws as ws;
+use rocket::serde::json::Json;
+use rocket::serde::{Serialize, Deserialize};
+use rocket_db_pools::Database;
+use rocket_db_pools::mongodb::Client;
+use rocket::futures::stream::TryStreamExt;
+
+#[derive(Database)]
+#[database("mongodb")]
+struct MongoDb(Client);
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Log {
+    id: i64,
+    content: String,
+}
 
 #[get("/")]
 fn get_index() -> &'static str {
@@ -31,23 +47,42 @@ fn websocket(ws: ws::WebSocket) -> ws::Stream!['static] {
     }
 }
 
-#[launch]
-fn rocket() -> _ {
-    let ssl_state = true; // Toogle for HTTPS and HTTP
-    let port = if ssl_state { 443 } else { 80 };
+#[get("/db")]
+async fn get_db(db: &MongoDb) -> Json<Vec<Log>> {
+    let database = db.database("DatabaseName");
+    let collection = database.collection::<Log>("CollectionName"); // <Log> here is a structure of CollectionName in Rust
 
-    let mut config = rocket::config::Config::default();
-    config.port = port;
-
-    if ssl_state {
-        config.tls = Some(rocket::config::TlsConfig::from_paths(
-            "certs/cert.pem",
-            "certs/key.pem",
-        ));
+    match collection.find(None, None).await {
+        Ok(cursor) => {
+            match cursor.try_collect::<Vec<Log>>().await { // Make sure Document exist in the database Based on <Log> Struct
+                Ok(logs) => {
+                    Json(logs)
+                },
+                Err(e) => {
+                    eprintln!("Error collecting logs: {:?}", e);
+                    Json(vec![])
+                }
+            }
+        },
+        Err(e) => {
+            eprintln!("Error querying MongoDB: {:?}", e);
+            Json(vec![])
+        }
     }
-
-    rocket::custom(config)
-      .mount("/", routes![get_index, post_index, websocket])
 }
 
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    // Config has been moved to Rocket.toml
+    // To Disable HTTPS just comment out default.tls and change The Port in Rocket.toml
+    // Database Config is also in Rocket.toml
 
+    let rocket = rocket::build()
+        .attach(MongoDb::init())
+        .mount("/", routes![get_index, post_index, websocket, get_db])
+        .ignite()
+        .await?;
+
+    rocket.launch().await?;
+    Ok(())
+}
